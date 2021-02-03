047254c8a3dfae4b2651f253b0e7724a793ae8c6
SAMARA33 <76135852+SAMARA33@users.noreply.github.com>
2021-02-03 18:27:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var images =
+   ["https://th.bing.com/th/id/OIP.p4rphPz7uUzX4MnK0VJ4hgHaB5w=323&h=89&c=7&o=5&pid=1.7","https://th.bing.com/th/id/OIP.orG4tjeZWWuSHLYZLV0aAAHaJa?w=115&h=150&c=7&o=5&pid=1.7",
+"https://th.bing.com/th/id/OIP.2nerPk5jQXSTxVkEoyBpYQHaLb?w=128&h=197&c=7&o=5&pid=1.7",
+"https://th.bing.com/th/id/OIP.nFcOiLTOmPapsh7h1A2SCQHaLH?w=137&h=180&c=7&o=5&pid=1.7",
+"https://th.bing.com/th/id/OIP.y4CxBUPlS56JUERu94hrWwFNC7?w=305&h=180&c=7&o=5&pid=1.7"];
+var names=["Family book","Tonny Pere","Caroline Pere","Austin Pere","Samara Pere"];
+var i=0;
+function updates()
+{
+  i++;
+  var
+  numbers_of_family_member_in_array_of_family_member_in_array=5
+  if(i > numbers_of_family_member_in_array );
+}
+i=0;
+  {
+    var updatedImage =
+        images[i];
+    var updatedName =
+        names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+    
+  }
